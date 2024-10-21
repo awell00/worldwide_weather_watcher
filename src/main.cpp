6807eb45a45f3da12c54a57b7332cb1e12f32eb3
robin <robin@derenty.net>
2024-10-21 09:54:50 +0200

    feat: change the timer functionning for button interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,20 @@
 #define GREEN_BUTTON_PIN 2
 
 byte actualMod = 0;
-byte lastMode = 0;
-volatile bool isCounting = false; // Flag pour le compteur de 5 secondes
+byte lastMod = 0;
+byte nbOfMinutes = 1;
 volatile byte varCompteur = 0;
-volatile bool firstLaunch = false;
+volatile byte secondCounter = 0;
 
 void changeMod()
 {
-    isCounting = true;
+    cli();
+    TIMSK2 = 0b00000001;      // Autoriser l'interruption locale du timer
+    bitClear (TCCR2A, WGM20); // WGM20 = 0
+    bitClear (TCCR2A, WGM21); // WGM21 = 0
+    sei();
     varCompteur = 0;
-    firstLaunch = true;
+    secondCounter = 0;
 }
 
 void setup() {
@@ -23,10 +27,14 @@ void setup() {
     PORTD |= (1 << PD3); // Set pullUp resistance on pin 3 (PORTD, bit 3, at 1)
 
     cli(); // Stop interrupts
-    bitClear (TCCR2A, WGM20); // WGM20 = 0
-    bitClear (TCCR2A, WGM21); // WGM21 = 0
-    TCCR2B = 0b00000110; // Clock / 256 so 16 micro-s and WGM22 = 0
-    TIMSK2 = 0b00000001; // authorize interrupt with TOIE2
+
+    TCCR2B = 0b00000110; // Clock / 256 soit 16 micro-s et WGM22 = 0
+
+    TCCR1A = 0;              // Mode normal
+    TCCR1B = (1 << CS12) | (1 << CS10); // Prescaler de 1024
+    TCNT1 = 0;               // Compteur à 0
+    TIMSK1 = (1 << TOIE1);   // Active l'interruption de débordement pour Timer1
+
     sei(); // Restart Interrupts
 
 
@@ -46,49 +54,48 @@ void loop() {
 }
 
 ISR(TIMER2_OVF_vect) {
-    static byte secondCounter = 0;
     TCNT2 = 256 - 250; // 250 x 16 µS = 4 ms
-    if (isCounting && firstLaunch) {secondCounter = 0; firstLaunch = false; } // When we press one of the buttons we set secondCounter to 0
-    if (isCounting)
+    if (varCompteur++ > 250)
     {
-        if (varCompteur++ > 250) { // 250 * 4 ms = 1 s
 
-            varCompteur = 0;
-            secondCounter++;
+        varCompteur = 0;
+        secondCounter++;
 
-            if (secondCounter >= 5)
+        if (secondCounter >= 5)
+        {
+            if (digitalRead(GREEN_BUTTON_PIN) == LOW)
             {
-                if (digitalRead(GREEN_BUTTON_PIN) == LOW)
+                if (actualMod == 0)
                 {
-                    if (actualMod == 0)
-                    {
-                        ecoMod();
-                    }
-                    else if (actualMod == 3)
+                    ecoMod();
+                }
+                else if (actualMod == 3)
+                {
+                    standardMod();
+                }
+            }
+            else if (digitalRead(RED_BUTTON_PIN) == LOW)
+            {
+                if (actualMod == 0 || actualMod == 3)
+                {
+                    maintenanceMod();
+                }
+                else if (actualMod == 2)
+                {
+                    if (lastMod == 0)
                     {
                         standardMod();
                     }
-                }
-                else if (digitalRead(RED_BUTTON_PIN) == LOW)
-                {
-                    if (actualMod == 0 || actualMod == 3)
+                    else
                     {
-                        lastMode = actualMod;
-                        maintenanceMod();
-                    }
-                    else if (actualMod == 2)
-                    {
-                        if (lastMode == 0)
-                        {
-                            standardMod();
-                        }
-                        else
-                        {
-                            ecoMod();
-                        }
+                        ecoMod();
                     }
                 }
             }
+            cli();
+            TIMSK2 = 0b00000000;
+            sei();
+            secondCounter=0;
         }
     }
 }
