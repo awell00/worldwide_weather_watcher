dc23163cea797736793a984d360b31a6d1da7280
robin <robin@derenty.net>
2024-10-21 14:48:21 +0200

    feat: add the ability to the function addValue to store in first place the data if the array is empty (actually not empty but full of -41 because it's an impossible value of captors)

--- src/utils.cpp
+++ src/utils.cpp
@@ -14,6 +14,11 @@ void addValue(float *oldTab, float newValue)
 {
     for (int i =0; i<(sizeof(*oldTab) -1); i++)
     {
+        if (oldTab[i] < -40)
+        {
+            oldTab[i] = newValue;
+            return;
+        }
         oldTab[i] = oldTab[i+1];
     }
     oldTab[sizeof(*oldTab)-1] = newValue;
